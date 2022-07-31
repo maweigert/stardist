66eb5ab9f95be3b4d918e7d95cdd892cdc92289f
Martin Weigert <maweigert@users.noreply.github.com>
2022-07-31 16:08:29 +0200

    more flows

--- stardist/geometry/geom2d.py
+++ stardist/geometry/geom2d.py
@@ -7,7 +7,7 @@ from skimage.draw import polygon
 from csbdeep.utils import _raise
 
 from ..matching import _check_label_array
-from ..lib.stardist2d import c_star_dist,  c_starflow2d_map, c_starflow2d
+from ..lib.stardist2d import c_star_dist, c_starflow2d_map, c_starflow2d,  c_starflow2d_map_float32
 from ..utils import path_absolute, _is_power_of_2, _normalize_grid
 
 
@@ -18,6 +18,13 @@ def starflow2d_map(flow, labels, mask = None, delta=.1, rounds=1, verbose=False)
     else:
         mask = mask.astype(np.int32)
     return c_starflow2d_map(flow.astype(np.float32), labels.astype(np.int32), mask, np.float32(delta), np.int32(rounds), np.int32(verbose))
+
+def starflow2d_map_float32(flow, arr, mask = None, delta=.1, rounds=1, verbose=False):
+    if mask is None:
+        mask = np.ones(arr.shape, np.int32)
+    else:
+        mask = mask.astype(np.int32)
+    return c_starflow2d_map_float32(flow.astype(np.float32), arr.astype(np.float32), mask, np.float32(delta), np.int32(rounds), np.int32(verbose))
 
 def starflow2d(dist, mask):
     return c_starflow2d(dist.astype(np.float32), mask.astype(np.int32))
--- stardist/lib/stardist2d.cpp
+++ stardist/lib/stardist2d.cpp
@@ -699,6 +699,77 @@ static PyObject *c_starflow2d_map(PyObject *self, PyObject *args)
 
     return PyArray_Return(dst);
 }
+
+static PyObject *c_starflow2d_map_float32(PyObject *self, PyObject *args)
+{
+
+    PyArrayObject *flow = NULL;
+    PyArrayObject *mask = NULL;
+    PyArrayObject *labels = NULL;
+    PyArrayObject *dst = NULL;
+
+    int verbose, rounds;
+    float delta;
+
+    if (!PyArg_ParseTuple(args, "O!O!O!fii", &PyArray_Type, &flow, &PyArray_Type, &labels, &PyArray_Type, &mask, 
+                          &delta, &rounds, &verbose))
+        return NULL;
+
+    npy_intp *dims = PyArray_DIMS(flow);
+    npy_intp dims_dst[2];
+    dims_dst[0] = dims[0];
+    dims_dst[1] = dims[1];
+
+    dst = (PyArrayObject *)PyArray_SimpleNew(2, dims_dst, NPY_FLOAT32);
+
+#ifdef __APPLE__    
+#pragma omp parallel for 
+#else
+#pragma omp parallel for schedule(dynamic) 
+#endif
+    for (int i = 0; i < dims[0]; i++)
+    {
+        for (int j = 0; j < dims[1]; j++)
+        {
+
+
+            *(int *)PyArray_GETPTR2(dst, i, j) = 0;
+
+            const int value = *(int *)PyArray_GETPTR2(mask, i, j);
+            if (value == 0)
+            {
+                *(int *)PyArray_GETPTR2(dst, i, j) = 0;
+            }
+            else
+            {
+                float y = i;
+                float x = j;
+
+
+                for (int n = 0; n < rounds; n++)
+                {
+                    int i2 = clip(int(y), 0, dims[0] - 1);
+                    int j2 = clip(int(x), 0, dims[1] - 1);
+
+                    const float dx = *(float *)PyArray_GETPTR3(flow, i2, j2, 0);
+                    const float dy = *(float *)PyArray_GETPTR3(flow, i2, j2, 1);
+                
+                    y += delta * dx;
+                    x += delta * dy;
+                }
+
+                int i2 = clip(int(y), 0, dims[0] - 1);
+                int j2 = clip(int(x), 0, dims[1] - 1);
+
+                *(float *)PyArray_GETPTR2(dst, i, j) += *(float *)PyArray_GETPTR2(labels, i2,j2 );
+
+                // *(int *)PyArray_GETPTR2(dst, i2, j2) = 1; 
+            }
+        }
+    }
+
+    return PyArray_Return(dst);
+}
 static PyObject *c_starflow2d(PyObject *self, PyObject *args)
 {
 
@@ -772,6 +843,7 @@ static struct PyMethodDef methods[] = {
     {"c_non_max_suppression_inds", c_non_max_suppression_inds, METH_VARARGS, "non-maximum suppression"},
     {"c_star_dist", c_star_dist, METH_VARARGS, "star dist calculation"},
     {"c_starflow2d_map", c_starflow2d_map, METH_VARARGS, "post processing"},
+    {"c_starflow2d_map_float32", c_starflow2d_map_float32, METH_VARARGS, "post processing"},
     {"c_starflow2d", c_starflow2d, METH_VARARGS, "flow"},
     {NULL, NULL, 0, NULL}
 
